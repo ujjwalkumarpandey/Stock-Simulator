ecab2aef20b24436e789cbea8d57f1055942a686
AmanGupta011 <aman.2003.ag@gmail.com>
2023-06-21 18:12:55 +0530

    autoBuy

--- src/database/db.js
+++ src/database/db.js
@@ -1,22 +1,22 @@
 var mysql = require("mysql2");
 const dotenv = require('dotenv');
-let result = dotenv.config(); 
+let result = dotenv.config();
 
 
 var db = mysql.createConnection({
-  host:  process.env.DB_HOST,
+  host: process.env.DB_HOST,
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DATABASE,
 });
 
-    db.connect(function (err) {
-      if (err) {
-        console.log("Error in db connectivity");
-      } else {
-        console.log("connected to database");
-      }
-    });
-    
-module.exports=db;
+db.connect(function (err) {
+  if (err) {
+    console.log("Error in db connectivity");
+  } else {
+    console.log("connected to database");
+  }
+});
+
+module.exports = db;
 
