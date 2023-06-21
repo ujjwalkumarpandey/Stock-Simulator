c24acc499b7db66d6115041893d0251790748a4c
AmanGupta011 <aman.2003.ag@gmail.com>
2023-06-21 14:46:54 +0530

    chang

--- src/router/loginauth.js
+++ src/router/loginauth.js
@@ -94,7 +94,7 @@ router.post("/login", (req, res) => {
         console.log("username password doesnot matched");
         req.flash("message", err);
         // req.flash("message", "username and password does not match");
-        res.redirect("login");
+        // res.redirect("login");
       } else {
         if (result[0]) {
           name = result[0].name
