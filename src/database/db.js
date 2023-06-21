9c42a81e209677531359f2ecdd9c7ae608563d0c
AmanGupta011 <aman.2003.ag@gmail.com>
2023-06-21 16:42:22 +0530

    chang

--- src/database/db.js
+++ src/database/db.js
@@ -8,7 +8,10 @@ var db = mysql.createPool({
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DATABASE,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 20,
+  queueLimit: 0
 });
 
     // db.connect(function (err) {
